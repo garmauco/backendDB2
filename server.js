fbac041d0664a54cf7fd7e777d2ce98a41d5b464
Garmauco <garmauco@gmail.com>
2021-01-24 20:43:45 -0500

    Actions get,post,put,delete

--- server.js
+++ server.js
@@ -16,21 +16,81 @@ var connString = "DRIVER={DB2};DATABASE=" + process.env.DB_DATABASE + ";UID=" + 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
-app.get('/', (req, res) => {
+app.get('/departaments', (req, res) => {
   //res.sendFile(__dirname + '/public/index.html')
-  console.log('get/')
 ibmdb.open(connString, function (err,conn) {
   if (err) return console.log("errrp"+err);
-  conn.query('SELECT * from store2.departament', function (err, data) {
+  conn.query('SELECT * from '+process.env.DB_SCHEMA+'.departament', function (err, data) {
     if (err) console.log(err);
     else console.log(data);
     res.render('index.ejs', { departaments: data })
     conn.close(function () {
-      console.log('done');
+      console.log('Listar Registros');
     });
   });
 });
 });
+
+app.get('/departaments/:id' , (req, res) => {
+  ibmdb.open(connString, function (err,conn) {
+    if (err) return console.log("errrp"+err);
+    conn.query('SELECT * from '+process.env.DB_SCHEMA+'.departament WHERE DEPTCODE =?',[req.params.id], function (err, data) {
+      console.log('SELECT * from '+process.env.DB_SCHEMA+'.departament WHERE DEPTCODE =?'+[req.params.id])
+      if (err) console.log(err);
+      else console.log(data);
+      res.render('index.ejs', { departaments: data })
+      conn.close(function () {
+        console.log('Registro consultado '+req.params.id);
+      });
+    });
+  });
+});
+
 app.post('/departaments', (req, res) => {
-  console.log(req.body)
+  console.log(req.body.nameDept)
+  ibmdb.open(connString, function (err,conn) {
+    if (err) return console.log("errrp"+err);
+    let dept = req.body
+    var sql="INSERT INTO "+process.env.DB_SCHEMA+".departament (DEPTCODE, DEPTNAME) VALUES ('"+dept.codeDept+"', '"+dept.nameDept+"')"
+    conn.query(sql, function (err, data) {
+      if (err) console.log(err);
+      else console.log(data);
+      res.redirect('/departaments')
+      conn.close(function () {
+        console.log('Registro Insertado correctamente');
+      });
+    });
+  });
 })
+
+app.put('/departaments', (req, res) => {
+  console.log(req.body.nameDept)
+  ibmdb.open(connString, function (err,conn) {
+    if (err) return console.log("errrp"+err);
+    let dept = req.body
+    var sql="UPDATE "+process.env.DB_SCHEMA+".departament SET DEPTNAME='"+dept.nameDept+"' WHERE DEPTCODE='"+dept.codeDept+"'"
+    conn.query(sql, function (err, data) {
+      if (err) console.log(err);
+      else{ console.log(data);
+      //res.redirect('/departaments')
+      }
+      conn.close(function () {
+        console.log('Registro '+dept.nameDept+' Actualizado correctamente');
+      });
+    });
+  });
+})
+
+app.delete('/departaments/:id' , (req, res) => {
+  ibmdb.open(connString, function (err,conn) {
+    if (err) return console.log("errrp"+err);
+    conn.query('DELETE FROM '+process.env.DB_SCHEMA+'.departament WHERE DEPTCODE =?',[req.params.id], function (err, data) {
+      if (err) console.log(err);
+      else console.log(data);
+      res.render('index.ejs', { departaments: data })
+      conn.close(function () {
+        console.log('Registro eliminado '+req.params.id);
+      });
+    });
+  });
+});
